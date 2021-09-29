162b14ac1d9b97a509a12aa14094ce6ec12afa09
lin-br <wes.rsilva@gmail.com>
2021-09-29 17:18:13 -0300

    test(SplitStrings): add three more tests

--- codewars/src/test/java/com/tilmais/kata/SplitStringByTwoLettersTest.java
+++ codewars/src/test/java/com/tilmais/kata/SplitStringByTwoLettersTest.java
@@ -10,22 +10,51 @@ class SplitStringByTwoLettersTest {
   @Test
   @DisplayName("Should split the string and the result should be completed with an underscore.")
   void shouldSplitStringAndGetOneStringWithUnderscore() {
-    var result = SplitStringByTwoLetters.split("abc");
+    var result = SplitStringByTwoLetters.split("LovePizza");
     assertThat(result)
         .isNotNull()
         .isNotEmpty()
-        .hasSize(3)
-        .containsExactly("ab", "c_");
+        .hasSize(5)
+        .containsExactly("Lo", "ve", "Pi", "zz", "a_");
   }
 
   @Test
-  @DisplayName("Should split the string into three groups each with two letters.")
-  void shouldSplitStringInThreeGroupsWithTwoLetters() {
-    var result = SplitStringByTwoLetters.split("abcdef");
+  @DisplayName("Should split the string into five groups each with two letters.")
+  void shouldSplitStringInFiveGroupsWithTwoLetters() {
+    var result = SplitStringByTwoLetters.split("HelloWorld");
     assertThat(result)
         .isNotNull()
         .isNotEmpty()
-        .hasSize(3)
-        .containsExactly("ab", "cd", "ef");
+        .hasSize(5)
+        .containsExactly("He", "ll", "oW", "or", "ld");
+  }
+
+  @Test
+  @DisplayName("Should get an empty array if the text is empty.")
+  void shouldGetEmptyArrayWhenTextIsEmpty() {
+    var result = SplitStringByTwoLetters.split("");
+    assertThat(result).isEmpty();
+  }
+
+  @Test
+  @DisplayName("Should split the text with spaces.")
+  void shouldSplitStringWithSpaces() {
+    var result = SplitStringByTwoLetters.split("I Love Pizza");
+    assertThat(result)
+        .isNotNull()
+        .isNotEmpty()
+        .hasSize(6)
+        .containsExactly("I ", "Lo", "ve", " P", "iz", "za");
+  }
+
+  @Test
+  @DisplayName("Should split the text with spaces and underscore.")
+  void shouldSplitStringWithSpacesAndUnderscore() {
+    var result = SplitStringByTwoLetters.split("Hello World");
+    assertThat(result)
+        .isNotNull()
+        .isNotEmpty()
+        .hasSize(6)
+        .containsExactly("He", "ll", "o ", "Wo", "rl", "d_");
   }
 }
