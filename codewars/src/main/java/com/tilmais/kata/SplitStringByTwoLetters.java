7f2c6b1c07711b26fe8faeb473fb4532e0c71f09
lin-br <wes.rsilva@gmail.com>
2021-09-30 09:26:42 -0300

    refactor(SplitStrings): add split from regex module

--- codewars/src/main/java/com/tilmais/kata/SplitStringByTwoLetters.java
+++ codewars/src/main/java/com/tilmais/kata/SplitStringByTwoLetters.java
@@ -1,11 +1,14 @@
 package com.tilmais.kata;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class SplitStringByTwoLetters {
 
+  private static final Pattern REGEX = Pattern.compile("\\.");
+
   public static String[] split(final String text) {
-    var strings = text.replaceAll("(..)", "$1.").split("\\.");
+    var strings = REGEX.split(text.replaceAll("(..)", "$1."));
     return Arrays.stream(strings)
         .filter(twoChars -> !twoChars.isEmpty() || !twoChars.isBlank())
         .map(twoChars -> twoChars.length() < 2 ? twoChars.concat("_") : twoChars)
